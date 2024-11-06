ab79276f05ce2f2b818a5161d0586f7a5c7d2f63
rauanb <rauan_b@hotmail.com>
2024-11-06 06:01:21 -0300

    adicionado js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,28 @@
 AOS.init();
-console.log('Olá, parcel!');
+
+// Definir momento do evento
+const eventDate = new Date("Jun 6, 2025 08:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const countDown = setInterval(function() {
+    // Definir momento atual
+    const now = new Date();
+    const nowTimeStamp = now.getTime();
+
+    // Definir tempo até o evento
+    const timeUntilEvent = eventTimeStamp - nowTimeStamp;
+
+    // Definir constantes auxiliares
+    const minInMS = 1000 * 60;
+    const hoursInMs = minInMS * 60;
+    const daysInMs = hoursInMs * 24;
+
+    // Quebra do tempo em unidades
+    const daysUntilEvent = Math.floor(timeUntilEvent / daysInMs);
+    const hoursUntilEvent = Math.floor((timeUntilEvent % daysInMs) / hoursInMs);
+    const minUntilEvent = Math.floor((timeUntilEvent % hoursInMs) / minInMS);
+    const secUntilEvent = Math.floor((timeUntilEvent % minInMS) / 1000);
+
+    // Substituir conteúdo do span
+    document.getElementById('contador').innerHTML = `O maior campeonato de Speedcubing que Rio Preto já viu começa em ${daysUntilEvent}d ${hoursUntilEvent}d ${minUntilEvent}m e ${secUntilEvent}s`;
+}, 1000);
